536dfe70a3c21677bf2f997ede9d53e7ee6c6b2e
Juan Fuenzalida <70103443+Jonepredator@users.noreply.github.com>
2021-02-21 21:06:36 +0100

    Added search function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-import Movie from './components/Movie';
+import Movie from "./components/Movie";
+import SearchContent from "./components/Movie";
 
 
 const apiKey = 'fbf0238042e831e97ad243b9cf994e70';
 const url = 'https://api.themoviedb.org/3';
 const nowPlayingUrl = `${url}/movie/now_playing?&api_key=${apiKey}`;
+const search_API = `${url}/search/movie?&api_key=${apiKey}&query=`;
 
 function App() {
    const [movies, setMovies] = useState([]);
+   const [searchedMovie, setSearchedMovie] = useState([]);
+   const [searchTerm, setSearchTerm] = useState('');
 
    useEffect(() => {
       fetch(nowPlayingUrl)
@@ -18,9 +22,37 @@ function App() {
          });
    }, []);
 
+   const handleOnSubmit = (e) => {
+      e.preventDefault();
+
+      fetch(search_API + searchTerm)
+         .then(res => res.json())
+         .then(data => {
+            console.log(data);
+            setSearchedMovie(data.results);
+         });
+      setSearchTerm('');
+   };
+
+   const handleOnChange = (e) => {
+      setSearchTerm(e.target.value);
+   };
+
    return (
       <>
-         <header>search bar will be here with an icon</header>
+         <header >
+            <div className='container' >
+               <form onSubmit={handleOnSubmit} >
+                  <input
+                     className='search'
+                     type="search"
+                     placeholder='Search...'
+                     value={searchTerm}
+                     onChange={handleOnChange}
+                  />
+               </form>
+            </div>
+         </header>
 
          <div className="headline">
             <h1>Now Playing</h1>
@@ -30,6 +62,14 @@ function App() {
 
             {movies.length > 0 && movies.slice(0, 6).map((movie) => (
                <Movie
+                  key={movie.id}
+                  {...movie} />
+            ))}
+         </div>
+
+         <div className='movie-container2' >
+            {searchedMovie.length > 0 && searchedMovie.slice(0, 12).map((movie) => (
+               <SearchContent
                   key={movie.id}
                   {...movie} />
             ))}
